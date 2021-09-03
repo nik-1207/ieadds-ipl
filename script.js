4ead7f896f7daacc28ab472bfeca81fc47f5e984
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-03 15:18:39 +0530

    asdfghh

--- script.js
+++ script.js
@@ -97,6 +97,91 @@ teamData = [{
     }
 ]
 
+function Router(routes) {
+    try {
+        if (!routes) {
+            throw "Invalid Routes";
+        }
+        this.constructor(routes);
+        this.init();
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+Router.prototype = {
+    routes: undefined,
+    rootElem: undefined,
+    constructor: function(routes) {
+        this.routes = routes;
+        this.rootElem = document.getElementById('app');
+    },
+    init: function() {
+        (function(scope, r) {
+            window.addEventListener('hashchange', function(e) {
+                scope.hasChanged(scope, r);
+            });
+        })(this, this.routes);
+        this.hasChanged(this, this.routes)
+    },
+    hasChanged: function(scope, r) {
+        if (window.location.hash.length > 0) {
+            for (var i = 0, length = r.length; i < length; i++) {
+                var route = r[i];
+                if (route.isActive(window.location.hash.substr(1))) {
+                    scope.gotoRoute(route.htmlName)
+                }
+            }
+
+        }
+    },
+    gotoRoute: function(htmlName) {
+        (function(scope) {
+            var url = "views/" + htmlName;
+            xhttp = new XMLHttpRequest();
+            xhttp.open('GET', url, true);
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    scope.rootElem.innerHTML = this.responseText
+                }
+            };
+            xhttp.send();
+        })(this)
+    }
+};
+
+function Route(name, htmlName, defaultRoute) {
+    try {
+        if (!name || !htmlName) {
+            throw "missing params"
+        }
+        this.constructor(name, htmlName, defaultRoute);
+    } catch (e) {
+        console.log(e)
+    }
+}
+Route.prototype = {
+    name: undefined,
+    htmlName: undefined,
+    default: undefined,
+    constructor: function(name, htmlName, defaultRoute) {
+        this.name = name;
+        this.htmlName = htmlName;
+        this.defaultRoute = defaultRoute;
+    },
+    isActive: function(hashedPath) {
+        return hashedPath.replace('#', '') === this.name;
+    }
+}
+
+function init() {
+    var router = new Router(
+        [
+            new Route('about', 'about.html'),
+        ]
+    );
+}
+init();
 fetch("https://cors-anywhere.herokuapp.com/https://ipl-t20.herokuapp.com/teams", {
     method: "GET",
 }).then((data) => { return data.json() }).then((res) => {
