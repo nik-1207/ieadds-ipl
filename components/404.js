cc680710c088d3e6b24e6701e6992515eab3d3b7
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-06 12:20:20 +0530

    completed

--- components/404.js
+++ components/404.js
@@ -1,6 +1,9 @@
 let NotFound = {
     render: async() => {
-        let view = `Not Found`
+        let view = `    <div id="not-found">
+        Page Not Found
+        <br> 404 Error
+    </div>`
         return view
     },
     after_render: async() => {}
